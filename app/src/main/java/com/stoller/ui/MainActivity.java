50c36781af59208bd9358bd6c9924a4f8a9e2e2d
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2015-05-02 10:19:28 +0200

    Initial commit - Story App

---
+++ app/src/main/java/com/stoller/ui/MainActivity.java
@@ -0,0 +1,40 @@
+package com.stoller.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.stoller.R;
+
+
+public class MainActivity extends Activity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.et_name);
+        mStartButton = (Button) findViewById(R.id.btn_start);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
+    }
+}
